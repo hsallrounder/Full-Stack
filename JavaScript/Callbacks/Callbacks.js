3ce0d0e09af3c158ca6f770e6aa60ec125844261
hsallrounder <hsallrounder@gmail.com>
2022-11-02 10:07:20 +0530

    Asynchronous Callbacks

--- JavaScript/Callbacks/Callbacks.js
+++ JavaScript/Callbacks/Callbacks.js
@@ -13,31 +13,29 @@ add(2,3,print)
 
 const fs=require("fs");
 
-
-
 // Asynchronous
 
-// function callback(err,data){
-//     if(!err) console.log(data);
-// }
+function callback(err,data){
+    if(!err) console.log(data);
+}
 
-// for(let i=1;i<=5;i++) {
-//     fs.readFile("q"+i+".txt",'utf-8',callback);
-// }
+for(let i=1;i<=5;i++) {
+    fs.readFile("q"+i+".txt",'utf-8',callback);
+}
 
 // Synchronous
 
-let count = 0;
-function print(err,data){
-    count += 1;
-    if(count<6){
-        if(!err){
-            console.log(data);
-        }
-        else{
-            console.log(err);
-        }
-    }
-    fs.readFile("q"+(count+1)+".txt",'utf-8',print);            
-}
-fs.readFile("q1.txt",'utf-8',print);
+// let count = 0;
+// function print(err,data){
+//     count += 1;
+//     if(count<6){
+//         if(!err){
+//             console.log(data);
+//         }
+//         else{
+//             console.log(err);
+//         }
+//     }
+//     fs.readFile("q"+(count+1)+".txt",'utf-8',print);            
+// }
+// fs.readFile("q1.txt",'utf-8',print);
