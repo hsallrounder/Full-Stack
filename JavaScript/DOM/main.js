37cf7cec20f4d8b5afdfda5d1a1ea4fc4b500612
hsallrounder <hsallrounder@gmail.com>
2022-11-10 11:35:49 +0530

    DOM

--- JavaScript/DOM/main.js
+++ JavaScript/DOM/main.js
@@ -26,3 +26,38 @@ console.log(document.all[12].innerText);
 let header=document.getElementById('header-title');
 
 header.style.backgroundColor='red';
+
+// header.innerText='123';
+
+// header.innerHTML = '<h3> This is a h3 heading</h3>';
+header.innerHTML = '<h3> hello world</h3>';
+
+console.log(header);
+
+// getElementByClassName
+
+var items = document.getElementsByClassName('list-group-item');
+
+console.log(items);
+
+for(let i=0;i<items.length;i++){
+
+    // items[i].style.backgroundColor='lightgrey'
+
+    Object.assign(items[i].style,{backgroundColor:'red',fontWeight:'bold'})
+}
+// getElementByQuerySelector
+
+/*
+var heading=document.querySelector('.title');
+
+heading.style.backgroundColor='green';
+*/
+
+var heading=document.querySelectorAll('.title');
+
+console.log(heading);
+
+for(let i=0;i<heading.length;i++){
+    heading[i].style.backgroundColor='green';
+}
