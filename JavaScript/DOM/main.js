6f951ffce6371539d2fb0878d860f782da79f044
hsallrounder <hsallrounder@gmail.com>
2022-11-10 10:51:37 +0530

    DOM

---
+++ JavaScript/DOM/main.js
@@ -0,0 +1,28 @@
+/*
+console.dir(document);
+
+console.log(document.domain);
+
+console.log(document.URL);
+
+console.log(document.body);
+
+console.log(document.head);
+
+console.log(document.links);
+
+console.log(document.all);
+
+console.log(document.all[5]);
+
+document.all[5].textContent='hello world';
+
+console.log(document.all[12].textContent);
+console.log(document.all[12].innerText);
+*/
+
+// Querying Document
+
+let header=document.getElementById('header-title');
+
+header.style.backgroundColor='red';
