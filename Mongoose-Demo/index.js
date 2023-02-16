d6c6a6c3620d322a6f69fbba226960bb55c7c347
hsallrounder <hsallrounder@gmail.com>
2023-02-16 11:51:08 +0530

    bekaar the bahi ye

--- Mongoose-Demo/index.js
+++ Mongoose-Demo/index.js
@@ -1,5 +1,26 @@
 const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://127.0.0.1:27017/seriesDB')
-.then(()=>console.log("connection open"))
-.catch((err)=> console.log("connection failed"))
+    .then(() => console.log("connection open"))
+    .catch((err) => console.log("connection failed", err))
+
+const seriesSchema = new mongoose.Schema({
+    name : String,
+    date : Number,
+    ratings : Number,
+    isWatched : Boolean
+})
+
+const Serie = mongoose.model('Serie', seriesSchema)
+
+// Technically model is a js class
+
+/*
+const friends = new Serie({ name:"friends reunion", date: 2018, ratings: 8.5, isWatched: true });
+
+console.log(friends);
+
+friends.save()
+    .then(()=> console.log("data stored in db"))
+
+*/
